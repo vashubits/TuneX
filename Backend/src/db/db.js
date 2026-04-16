82c7b5eb9bc90dead08d0fad36e3fe6a1a6584a1
vashubits <cse22_vashugupta@ggct.co.in>
2026-04-16 21:16:26 +0530

    Initial commit - Music Player project

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose')
-
-
 async function connectDB() {
-   await mongoose.connect('mongodb+srv://V4shu:BefCFYNs35FaaUAx@cluster0.59d60tt.mongodb.net/Music_Player')
-   console.log("mongodb connection successfull")
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("MongoDB connection successful");
 }
-
-module.exports = connectDB
