e41ace9df18ede3e11013662f5c07d3c552a29f6
vashubits <cse22_vashugupta@ggct.co.in>
2026-04-24 22:44:27 +0530

    Initial commit - Music Player project

--- Backend/src/middleware/register.validater.js
+++ Backend/src/middleware/register.validater.js
@@ -25,7 +25,7 @@ const validate = (req, res, next) => {
 
   if (!errors.isEmpty()) {
     return res.status(400).json({
-      errors: errors.array()
+      errors: errors.array()[0].msg
     })
   }
 
